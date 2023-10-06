5136fc7e3d26150467ce73bd9bb1db2d4bed7776
Ankit <guptashirley02@gmail.com>
2023-10-06 10:55:02 +0530

    .

--- middleware.ts
+++ middleware.ts
@@ -8,6 +8,7 @@ export default authMiddleware({
     ignoredRoutes: ['/api/webhook/clerk'] 
 });
  
+
 export const config = {
       matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
